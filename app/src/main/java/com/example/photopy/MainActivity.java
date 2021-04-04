eb097d42f04c153b1bcfc4e6b2a4fb1eb7a7c8ae
ismailrisky <ismailrisky01@gmail.com>
2021-04-04 14:42:38 +0700

    create blank fragment

--- app/src/main/java/com/example/photopy/MainActivity.java
+++ app/src/main/java/com/example/photopy/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.photopy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_add, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+               switch (destination.getId()){
+                   case R.id.loginFragment:navView.setVisibility(View.GONE); break;
+                   case R.id.navigation_home:
+                   case R.id.navigation_profile:
+                   case R.id.navigation_add:
+                       navView.setVisibility(View.VISIBLE); break;
+
+               }
+            }
+        });
     }
 }
